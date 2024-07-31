988b2eedbad864ad48fcc0b9f2c15d36e58c9ce4
Smr021 <161032439+Smr021@users.noreply.github.com>
2024-07-31 11:40:11 -0300

    Add files via upload

---
+++ NetBeansProjects/LojaMamaoComAcuca/src/main/java/com/mycompany/lojamamaocomacuca/LojaMamaoComAcuca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lojamamaocomacuca;
+
+/**
+ *
+ * @author Aluno
+ */
+public class LojaMamaoComAcuca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ NetBeansProjects/Operadores/src/main/java/com/mycompany/operadores/Operadores.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operadores;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Operadores {
+    
+    public static void main(String[] args) {
+    //Declaração de variávies
+    int a = 10;
+    int b = 2;
+    
+    //Operadores aritmétricos
+    int multiplicar = a * b ;
+    int dividir = a / b;
+    int somar = a + b;
+    int subtrair = a - b;
+    
+    //Exibindo resultados 
+    System.out.println("Multiplicar: " + multiplicar);
+    System.out.println("Dividir: " + dividir);
+    System.out.println("Somar: " + somar);
+    System.out.println("Subtrair " + subtrair);
+    }
+}
